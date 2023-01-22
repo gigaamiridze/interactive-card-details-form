fc47c7621843bd9fb6a95735895d8f0a0031f2b6
justtfelix <justttfelix@gmail.com>
2023-01-22 21:23:49 +0400

    Added card front side content and its styles

--- src/components/CardFrontSide/CardFrontSide.tsx
+++ src/components/CardFrontSide/CardFrontSide.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import cardLogo from '../../assets/svg/card-logo.svg';
 
 function CardFrontSide() {
   return (
     <div className='card-front'>
-
+      <img src={cardLogo} className='card-logo' alt="Card Logo" />
+      <div>
+        <span className='card-number'>0000 0000 0000 0000</span>
+        <div className='flex-box'>
+          <span className='name'>jane appleseed</span>
+          <span>00/00</span>
+        </div>
+      </div>
     </div>
   )
 }
