90e7df1b5f68e0009fb3aaf0585c34fa3a6fbce8
justtfelix <justttfelix@gmail.com>
2023-01-22 20:08:43 +0400

    Added main background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import mainBg from './assets/image/bg-main-desktop.png';
 
 function App() {
   return (
     <div className="App">
-
+      <img src={mainBg} className='main-bg' alt="Main Bg" />
     </div>
   )
 }
