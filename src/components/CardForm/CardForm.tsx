16d0cf2f2f02cf17fbcf7c669cbc0ee4a542ad4c
justtfelix <justttfelix@gmail.com>
2023-01-24 13:02:54 +0400

    Added styles of input fields

--- src/components/CardForm/CardForm.tsx
+++ src/components/CardForm/CardForm.tsx
@@ -5,22 +5,22 @@ function CardForm() {
     <form>
       <div className='form-group'>
         <label htmlFor="name">cardholder name</label>
-        <input type="text" name="name" id="name" />
+        <input type="text" placeholder="e.g. Jane Appleseed" name="name" id="name" />
       </div>
       <div className='form-group'>
         <label htmlFor="card-number">card number</label>
-        <input type="text" name="card-number" id="card-number" />
+        <input type="text" placeholder="e.g. 1234 5678 9123 0000" name="card-number" id="card-number" />
       </div>
       <div className='form-group'>
-        <div className='flex-box'>
+        <div className='wrapper-box'>
           <label htmlFor="exp-date">exp. date (MM/YY)
             <div className='flex-box'>
-              <input type="text" name="exp-date" id="exp-date" />
-              <input type="text" name="exp-date" id="exp-date" />
+              <input type="text" placeholder="MM" name="exp-date" id="exp-date" />
+              <input type="text" placeholder="YY" name="exp-date" id="exp-date" />
             </div>
           </label>
           <label htmlFor="cvc">cvc
-            <input type="text" name="cvc" id="cvc" />
+            <input type="text" placeholder="e.g. 123" name="cvc" id="cvc" />
           </label>
         </div>
       </div>
